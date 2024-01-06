3723ddf94d9ffcec4823cc95b7ec7a36635f71cf
Nikunj Aggarwal <aggarwalnikunj1999@gmail.com>
2024-01-06 17:29:02 +0530

    Learned about go routines and channels

--- go-routines/main.go
+++ go-routines/main.go
@@ -5,18 +5,37 @@ import (
 	"time"
 )
 
-func greet(s string) {
+func greet(s string, done_channel chan bool) {
 	fmt.Println(s)
+
+	if done_channel != nil {
+		done_channel <- true
+	}
 }
 
-func slowGreet(s string) {
+func slowGreet(s string, done_channel chan bool) {
 	time.Sleep(time.Second * 3)
 	fmt.Println(s)
+
+	done_channel <- true
+	close(done_channel)
 }
 
 func main() {
-	go greet("Hello")
-	go greet("How are you?")
-	go slowGreet("How... are... you...?")
-	go greet("I am fine")
+	var doneChannel = make(chan bool)
+
+	go greet("Hello", doneChannel)
+	go greet("How are you?", doneChannel)
+	go slowGreet("How... are... you...?", doneChannel)
+	go greet("I am fine", doneChannel)
+	<-doneChannel
+	go slowGreet("How... are... you... (2)?", doneChannel)
+	go greet("Done with slow Greet?", doneChannel)
+	go greet("Last!!", doneChannel)
+	<-doneChannel
+	<-doneChannel
+	<-doneChannel
+	<-doneChannel
+	<-doneChannel
+	<-doneChannel
 }
